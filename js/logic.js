3e4a84f9e9864a6a6f99d1f092d882a77596097b
CreativeQuality <info@creativequality.dev>
2023-11-27 21:21:15 +0100

    increase "about" font and simplify some more

--- js/logic.js
+++ js/logic.js
@@ -1,18 +1,15 @@
 "use strict";
 
 function toggle(name) {
-	let buttonCells = document.querySelectorAll("span.button-cell");
-	for (const buttonCell of buttonCells) {
+	for (const buttonCell of document.querySelectorAll("span.button-cell")) {
 		if (buttonCell.classList.contains(name))
 			buttonCell.classList.add("toggled");
 		else
 			buttonCell.classList.remove("toggled");
 	}
 
-	let contentDivs = document.querySelectorAll("div.content");
-	for (const contentDiv of contentDivs) {
-		let displayStyle = contentDiv.classList.contains(name) ? "block" : "none";
-		contentDiv.style.display = displayStyle;
+	for (const contentDiv of document.querySelectorAll("div.content")) {
+		contentDiv.style.display = contentDiv.classList.contains(name) ? "block" : "none";
 	}
 }
 
