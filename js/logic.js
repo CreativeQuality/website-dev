38c21704e901c3facca0b6dc77065ff062c8ef78
CreativeQuality <info@creativequality.dev>
2023-11-27 21:15:09 +0100

    simplify logic

--- js/logic.js
+++ js/logic.js
@@ -1,31 +1,19 @@
 "use strict";
 
 function toggle(name) {
-	for (const buttonCell of $("span.button-cell")) {
+	let buttonCells = document.querySelectorAll("span.button-cell");
+	for (const buttonCell of buttonCells) {
 		if (buttonCell.classList.contains(name))
 			buttonCell.classList.add("toggled");
 		else
 			buttonCell.classList.remove("toggled");
 	}
 
-	for (const contentDiv of $("div.content")) {
+	let contentDivs = document.querySelectorAll("div.content");
+	for (const contentDiv of contentDivs) {
 		let displayStyle = contentDiv.classList.contains(name) ? "block" : "none";
 		contentDiv.style.display = displayStyle;
 	}
-}
-
-/**
- * Below helper functions are from https://plainjs.com/
- */
-
-// select a list of matching elements, context is optional
-function $(selector, context) {
-    return (context || document).querySelectorAll(selector);
-}
-
-// select the first match only, context is optional
-function $1(selector, context) {
-    return (context || document).querySelector(selector);
 }
 
 window.onload = () => toggle("about");
