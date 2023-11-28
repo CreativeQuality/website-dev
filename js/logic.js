7ac95a676ac1e51a0f00eb41721f159b66750cec
CreativeQuality <info@creativequality.dev>
2023-11-28 11:53:48 +0100

    improve JS readability

--- js/logic.js
+++ js/logic.js
@@ -1,16 +1,12 @@
 "use strict";
 
 function toggle(name) {
-	for (const button of document.querySelectorAll("span.button")) {
-		if (button.id == "button-" + name)
-			button.classList.add("toggled");
-		else
-			button.classList.remove("toggled");
-	}
+	for (const button of document.querySelectorAll("span.button"))
+		if (button.id == "button-" + name) button.classList.add("toggled");
+		else button.classList.remove("toggled");
 
-	for (const contentDiv of document.querySelectorAll("div.content")) {
+	for (const contentDiv of document.querySelectorAll("div.content"))
 		contentDiv.style.display = contentDiv.id == "content-" + name ? "block" : "none";
-	}
 }
 
 window.onload = () => toggle("about");
